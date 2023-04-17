410bab6040bae0e0bfe0e3fd2a5fd2b5b0becd15
Seph-23 <seongmlyu95@gmail.com>
2023-04-17 16:24:04 +0900

    feat: API 개발 기본 개념

--- jpashop/src/main/java/jpabook/jpashop/domain/Member.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -8,6 +8,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.validation.constraints.NotEmpty;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import lombok.Getter;
 import lombok.Setter;
 
@@ -24,6 +28,7 @@ public class Member {
   @Embedded
   private Address address;
 
+  // @JsonIgnore
   @OneToMany(mappedBy = "member")
   private List<Order> orders = new ArrayList<>();
 }
